519c2d0aba03071a4f301176f7e5f0d193e906fb
smastrom <60471784+smastrom@users.noreply.github.com>
2023-07-11 03:45:38 +0200

    add demo

---
+++ demo/App.vue
@@ -0,0 +1,72 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+import { useFixedHeader } from '../src/useFixedHeader'
+
+const headerRef = ref<HTMLElement | null>(null)
+
+const containerRef = ref<HTMLElement | null>(null)
+const headerContainerRef = ref<HTMLElement | null>(null)
+
+const { isVisible, delta } = useFixedHeader(headerRef, {
+   delta: 0.5,
+})
+
+useFixedHeader(headerContainerRef, {
+   root: containerRef,
+   delta: 0.5,
+})
+</script>
+
+<template>
+   <div>
+      <header class="Header" ref="headerRef">
+         {{ delta }} {{ isVisible }}
+
+         <h3>Vue 3 + Vite + TypeScript + Tailwind CSS</h3>
+      </header>
+
+      <div class="Page"></div>
+
+      <div class="Container_Scroll" ref="containerRef">
+         <div class="Container">
+            <header class="HeaderContainer" ref="headerContainerRef">
+               {{ delta }} {{ isVisible }}
+
+               <h3>Vue 3 + Vite + TypeScript + Tailwind CSS</h3>
+            </header>
+         </div>
+      </div>
+   </div>
+</template>
+
+<style scoped>
+.Header {
+   background-color: white;
+   width: 100%;
+   height: 80px;
+   top: 0;
+   position: sticky;
+}
+.Page {
+   height: 4000px;
+}
+
+.Container_Scroll {
+   height: 400px;
+   overflow: auto;
+   background-color: white;
+}
+
+.Container {
+   height: 1800px;
+   position: relative;
+   background-color: white;
+}
+
+.HeaderContainer {
+   height: 60px;
+   position: sticky;
+   top: 0;
+   background-color: aquamarine;
+}
+</style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,39 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+import vue from '@vitejs/plugin-vue'
+import dts from 'vite-plugin-dts'
+import terser from '@rollup/plugin-terser'
+
+export default defineConfig(({ mode }) => {
+   if (mode === 'demo') return { plugins: [vue()] }
+
+   return {
+      build: {
+         lib: {
+            entry: 'src/useFixedHeader.ts',
+            name: 'vue-use-fixed-header',
+            fileName: 'index',
+            formats: ['es', 'cjs'],
+         },
+         rollupOptions: {
+            external: ['vue'],
+            output: {
+               globals: {
+                  vue: 'Vue',
+               },
+            },
+            plugins: [
+               terser({
+                  compress: {
+                     drop_console: true,
+                     defaults: true,
+                     passes: 4,
+                     ecma: 2020,
+                  },
+               }),
+            ],
+         },
+      },
+      plugins: [dts({ staticImport: true, insertTypesEntry: true }), vue()],
+   }
 })
